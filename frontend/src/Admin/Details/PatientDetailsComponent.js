e49af94013232af8c0d730ee1e077cac4027e85b
EziukuLigonine <35303583+EziukuLigonine@users.noreply.github.com>
2018-02-23 14:38:39 +0200

    fix'as

--- frontend/src/Admin/Details/PatientDetailsComponent.js
+++ frontend/src/Admin/Details/PatientDetailsComponent.js
@@ -1,34 +1,8 @@
 import React from 'react';
 
 const PatientDetailsComponent = (props) => {
-<<<<<<< HEAD
-    const styles = {
-        margin: '20px'
-    }
 
-    var assignDoctor = (event) => {
-        props.history.push("/admin/patient/assign/" + props.patient.id);
-        event.preventDefault();
-    };
-    return (
-        <div className="row">
-            <div className="col-md-3">
-                <div className="thumbnail" style={styles}>
-                    <img src="/user.png" alt="..." />
-                    <div className="caption">
-                        <h3>{props.patient.name} {props.patient.surname}</h3>
-                        <p>Vartotojo ID: {props.patient.id}</p>
-                        <p>Vartotojo vardas: {props.patient.username}</p>
-                        <p>Asmens kodas: {props.patient.personalId}</p>
-                        <p>Gimimo data: {props.patient.dateOfBirth}</p>
-                        <p>Gydytojas: {props.patient.doctorUsername}</p>
-                        <button type="submit" className="btn btn-success" onClick={assignDoctor}>Priskirti gydytoją</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-=======
+
  const styles = {
    margin: '20px'
  }
@@ -55,7 +29,7 @@ const PatientDetailsComponent = (props) => {
       </div>
     </div>
   );
->>>>>>> frontend
+
 };
 
 export default PatientDetailsComponent;
