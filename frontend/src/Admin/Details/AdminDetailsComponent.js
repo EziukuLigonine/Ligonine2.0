c44d6a0fc83e3ff6a6a6bb72de3a5829194f9d15
EziukuLigonine <35303583+EziukuLigonine@users.noreply.github.com>
2018-02-23 15:18:40 +0200

    fix'as

--- frontend/src/Admin/Details/AdminDetailsComponent.js
+++ frontend/src/Admin/Details/AdminDetailsComponent.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
 const AdminDetailsComponent = (props) => {
-<<<<<<< HEAD
-    const styles = {
-        margin: '20px'
-    }
-    return (
-        <div className="row">
-            <div className="col-md-3">
-                <div className="thumbnail" style={styles}>
-                    <img src="/user.png" alt="..." />
-                    <div className="caption">
-                        <h3>{props.admin.name} {props.admin.surname}</h3>
-                        <p>Vartotojo ID: {props.admin.id}</p>
-                        <p>Vartotojo vardas: {props.admin.username}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-=======
+
  const styles = {
    margin: '20px'
  }
@@ -37,7 +19,7 @@ const AdminDetailsComponent = (props) => {
       </div>
     </div>
   );
->>>>>>> frontend
+
 };
 
 export default AdminDetailsComponent;
--- frontend/src/Admin/Details/DoctorDetailsComponent.js
+++ frontend/src/Admin/Details/DoctorDetailsComponent.js
@@ -1,26 +1,7 @@
 import React from 'react';
 
 const DoctorDetailsComponent = (props) => {
-<<<<<<< HEAD
-    const styles = {
-        margin: '20px'
-    }
-    return (
-        <div className="row">
-            <div className="col-md-3">
-                <div className="thumbnail" style={styles}>
-                    <img src="/user.png" alt="..." />
-                    <div className="caption">
-                        <h3>{props.doctor.name} {props.doctor.surname}</h3>
-                        <p>Vartotojo ID: {props.doctor.id}</p>
-                        <p>Vartotojo vardas: {props.doctor.username}</p>
-                        <p>Specializacija: {props.doctor.specialisation}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-=======
+
  const styles = {
    margin: '20px'
  }
@@ -39,7 +20,7 @@ const DoctorDetailsComponent = (props) => {
       </div>
     </div>
   );
->>>>>>> frontend
+
 };
 
 export default DoctorDetailsComponent;
