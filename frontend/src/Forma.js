ec6540be4b0450d519b962f37c1f57ec467599e8
Paulius <paulius.vistejunas@gmail.com>
2018-03-02 10:21:19 +0200

    Login form edit

--- frontend/src/Forma.js
+++ frontend/src/Forma.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import './Forma.css';
 const Forma = ({username, password, onPassChange, onUsernameChange, onSubmit}, context) => {
   return(
-    <form onSubmit={onSubmit}>
-      <input type="text" value={username} onChange={onUsernameChange} />
-      <input type="password" value={password} onChange={onPassChange} />
-      <input type="submit" />
-    </form>
+    <div className="row">
+      <div className="col-md-3">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Prisijunkite</h3>
+          </div>
+          <div className="panel-body">
+            <form onSubmit={onSubmit}>
+              Vartotojo vardas:<br/>
+              <input type="text" value={username} onChange={onUsernameChange} />
+              Slaptažodis:
+              <input type="password" value={password} onChange={onPassChange} />
+              <br/>
+              <br/>
+              <button type="submit" className="btn btn-success">Prisijungti</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
