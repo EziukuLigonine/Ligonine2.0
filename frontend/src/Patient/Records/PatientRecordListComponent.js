981807a666f2ddd8a9c7234b775d13d98ad54e15
Paulius <paulius.vistejunas@gmail.com>
2018-03-04 21:19:58 +0200

    patient can see record details

--- frontend/src/Patient/Records/PatientRecordListComponent.js
+++ frontend/src/Patient/Records/PatientRecordListComponent.js
@@ -3,8 +3,8 @@ import React from 'react';
  const PatientPrescriptionListComponent = (props) => {
   var {id, personalId, duration, tlk, appDesc, vlk, repeated, doctorUsername, date} = props;
 
-  var getPrescription = (event) => {
-      props.history.push("/patient/prescription/" + id);
+  var getRecord = (event) => {
+      props.history.push("/patient/record/" + id);
       event.preventDefault();
   };
 
@@ -14,7 +14,7 @@ import React from 'react';
         <td>{tlk}</td>
         <td>{doctorUsername}</td>
         <td>
-          <span id={id} className="glyphicon glyphicon-eye-open" aria-hidden="true" onClick={getPrescription}></span>
+          <span id={id} className="glyphicon glyphicon-eye-open" aria-hidden="true" onClick={getRecord}></span>
         </td>
       </tr>
   );
